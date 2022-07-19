01e624197a59b07b91cfce6b15ed13cd0b0277d3
adamapplegate <applegateadaml@gmail.com>
2022-07-19 08:14:08 -0600

    Added utility for calculating a sha256 checksum

--- BirdIdentifier/Utils/ChecksumUtils.cs
+++ BirdIdentifier/Utils/ChecksumUtils.cs
@@ -1,6 +1,16 @@
-﻿namespace BirdIdentifier.Utils;
+﻿using System.Security.Cryptography;
 
-public class ChecksumUtils
+namespace UploadImage_Api.Util;
+
+public static class ChecksumUtils
 {
-    
+    public static string GetChecksum(IFormFile file)
+    { 
+        using (var stream = file.OpenReadStream())
+        {
+            var sha = new SHA256Managed();
+            var checksum = sha.ComputeHash(stream);
+            return BitConverter.ToString(checksum).Replace("-", string.Empty);
+        }
+    }
 }
