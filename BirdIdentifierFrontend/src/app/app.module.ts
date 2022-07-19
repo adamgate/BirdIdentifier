400d0a5e3e71b9115dd22efc8d28d0471a41bf13
adamapplegate <applegateadaml@gmail.com>
2022-07-19 14:17:33 -0600

    Generated necessary pieces

--- BirdIdentifierFrontend/src/app/app.module.ts
+++ BirdIdentifierFrontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImageUploadComponent } from './image-upload/image-upload.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ImageUploadComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
