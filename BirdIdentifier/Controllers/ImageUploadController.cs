5ff365d04973fc798ee6c84059ee21cf7c94e777
adamapplegate <applegateadaml@gmail.com>
2022-07-19 13:17:29 -0600

    actually fixed dependency issues

--- BirdIdentifier/Controllers/ImageUploadController.cs
+++ BirdIdentifier/Controllers/ImageUploadController.cs
@@ -13,40 +13,36 @@ public class ImageUploadController : ControllerBase
     {
         var fileExt = Path.GetExtension(file.FileName);
 
-        //Check for the correct filetypes
-        if (fileExt == ".jpg" ||
-            fileExt == ".jpeg" ||
-            fileExt == ".png")
-        {
-            //Create a checksum used as the file name to avoid duplicate files
-            var checksum = ChecksumUtils.GetChecksum(file);
-
-            //Create directory if it doesn't exist
-            Directory.CreateDirectory(@"./UploadedImages");
-            var filePath = $@"./UploadedImages/{checksum}{fileExt}";
-            
-            // Save the file if it isn't already present
-            if (!System.IO.File.Exists(filePath))
-            {
-                await using (var stream = System.IO.File.Create(filePath))
-                {
-                    await file.CopyToAsync(stream);
-                }
-            }
-
-            //Convert image into format the ML model can use
-            var mlData = new MLModel.ModelInput
-            {
-                ImageSource = filePath
-            };
-            
-            //Pass in the data and get a prediction
-            var predictionResult = MLModel.Predict(mlData);
-
-                //Return prediction to user
-            return Ok(predictionResult.Prediction);
-        }
+        //Check for the correct filetypes & return error if they don't match
+        if (fileExt != ".jpg" && fileExt != ".jpeg" && fileExt != ".png")
+            return BadRequest("File was not of type .png, .jpg, or .jpeg.");
         
-        return BadRequest("File was not of type .png, .jpg, or .jpeg.");
+        //Create a checksum from the image to avoid duplicate files
+        var checksum = ChecksumUtils.GetChecksum(file);
+
+        //Create directory if it doesn't exist
+        Directory.CreateDirectory(@"./UploadedImages");
+        var filePath = $@"./UploadedImages/{checksum}{fileExt}";
+            
+        // Save the file if it isn't already present
+        if (!System.IO.File.Exists(filePath))
+        {
+            await using (var stream = System.IO.File.Create(filePath))
+            {
+                await file.CopyToAsync(stream);
+            }
+        }
+
+        //Convert image into format the ML model can use
+        var mlData = new MLModel.ModelInput
+        {
+            ImageSource = filePath
+        };
+            
+        //Pass in the data and get a prediction
+        var predictionResult = MLModel.Predict(mlData);
+
+        //Return prediction to the front end
+        return Ok(predictionResult.Prediction);
     }
 }
