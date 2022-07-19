52aa14948c6351ffee376098d0bdbcf64145d0e3
adamapplegate <applegateadaml@gmail.com>
2022-07-19 08:15:07 -0600

    Changed project settings

--- BirdIdentifier/Program.cs
+++ BirdIdentifier/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add prediction pool for ml model
+// builder.Services.AddPredictionEnginePool<MLModel.ModelInput, MLModel.ModelOutput>()
+//     .FromFile(@"MLModel/MLModel.zip");
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
