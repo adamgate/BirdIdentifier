28102c886185e1507e6601cf93faecc9039a01a9
adamapplegate <applegateadaml@gmail.com>
2022-07-19 08:14:52 -0600

    Updated controller to accept an image, calculate a checksum to determine if it already exists, save it, and process with ml model

--- BirdIdentifier/Controllers/ImageUploadController.cs
+++ BirdIdentifier/Controllers/ImageUploadController.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UploadImage_Api.Util;
 
-namespace BirdIdentifier.Controllers;
+namespace UploadImage_Api.Controllers;
 
-public class ImageUploadController : Controller
+[ApiController]
+[Route("images")]
+public class ImageUploadController : ControllerBase
 {
-    // GET
-    public IActionResult Index()
+    [HttpPost]
+    public async Task<IActionResult> OnPostUpload(IFormFile file)
     {
-        return View();
+        var fileExt = Path.GetExtension(file.FileName);
+
+        //Check for the correct filetypes
+        if (fileExt == ".jpg" ||
+            fileExt == ".jpeg" ||
+            fileExt == ".png")
+        {
+            //Create a checksum used as the file name to avoid duplicate files
+            var checksum = ChecksumUtils.GetChecksum(file);
+
+            var filePath = $@"./UploadedImages/{checksum}{fileExt}";
+
+            //save file to the UploadedImages folder
+            await using (var stream = System.IO.File.Create(filePath))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            //Convert image into format the ML model can use
+            var mlData = new MLModel.ModelInput
+            {
+                ImageSource = filePath
+            };
+
+            //Pass in the data and get a prediction
+            var predictionResult = MLModel.Predict(mlData);
+
+            //Return prediction to user
+            return Ok(predictionResult.Prediction);
+        }
+
+        return NotFound();
     }
 }
