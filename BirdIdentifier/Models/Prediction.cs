5ae03c4533335fc119da21e46298d6c619577917
adamapplegate <applegateadaml@gmail.com>
2022-07-24 13:26:48 -0600

    Added new classes for tracking predictions and prediction accuracy

---
+++ BirdIdentifier/Models/Prediction.cs
@@ -0,0 +1,16 @@
+﻿namespace BirdIdentifier.Models;
+
+public class Prediction
+{
+    private int PredictionId { get; set; }
+    private int ImageChecksum { get; set; }
+    private string ImageBase64 { get; set; }
+    private DateTime Timestamp { get; set; }
+    private string PredictionName { get; set; }
+    private float PredictionScore { get; set; }
+    private string Link { get; set; }
+
+    public Prediction()
+    {
+    }
+}
---
+++ BirdIdentifier/Models/PredictionRating.cs
@@ -0,0 +1,13 @@
+﻿namespace BirdIdentifier.Models;
+
+public class PredictionRating
+{
+    private int PredictionRatingId { get; set; }
+    private bool WasCorrect { get; set; }
+    private string? CorrectPrediction { get; set; }
+    private string Description { get; set; }
+
+    public PredictionRating()
+    {
+    }
+}
